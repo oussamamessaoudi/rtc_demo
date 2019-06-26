18d142e3df5a480f681503e1f64c018403fad03a
u958287 <oussamamessaoudi@pca.ma>
2019-06-26 10:53:05 +0100

    Refactoring code

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React, {Component} from 'react';
+import {StreamState} from './StreamState'
+
 
 class App extends Component {
 
   constructor(props) {
     super(props);
-    App.hasUserMedia.bind(this);
-
-    this.state={
-      localStream : null
+    this.localStream = null;
+    this.state = {
+      localStreamStatus: StreamState.INITIAL
     }
   }
-
 
   static hasUserMedia() {
     navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia
@@ -19,28 +19,44 @@ class App extends Component {
     return navigator.getUserMedia;
   }
 
+  setLocalStream(localStreamStatus, localStream) {
+    this.localStream = localStream;
+    this.setState({localStreamStatus: StreamState.CONNECTED});
+  }
+
   componentDidMount() {
     if (App.hasUserMedia()) {
-      navigator.getUserMedia({ video: true, audio: true },
-         (stream) => this.setState({localStream : stream}),
-          error => console.log(error) );
-    }
-    else {
-      alert("Error. WebRTC is not supported!");
+      navigator.getUserMedia({video: true, audio: true},
+        (stream) => this.setLocalStream(StreamState.CONNECTED, stream),
+        (error) => this.setLocalStream(StreamState.PERMISSION_DENIED));
+    } else {
+      this.setLocalStream(StreamState.NOT_SUPPORTED);
     }
   }
 
 
   render() {
+    const {localStreamState} = this.state;
+    switch (localStreamState) {
+      case StreamState.INITIAL :
+        return <div>INITIAL</div>;
+      case StreamState.FAILED :
+        return <div>FAILED</div>;
+      case StreamState.CONNECTED :
+        return this.getConnectedFrame();
+      case StreamState.NOT_SUPPORTED :
+        return <div>FAILED</div>;
+      case StreamState.PERMISSION_DENIED :
+        return <div>PERMISSION_DENIED</div>;
+      default :
+        return <div>DEFAULT</div>
+    }
+  }
+
+  getConnectedFrame() {
     return (
       <div className="App">
-        <video autoPlay ref={
-          video => {
-            if(video){
-              video.srcObject = this.state.localStream
-            }
-          }
-        }/>
+        <video autoPlay ref={video => video.srcObject = this.localStream}/>
       </div>
     );
   }
